939a8449a76f296299393868e260deac30bf3c0d
ethan joshua <ethanjoh77@gmail.com>
2026-05-16 05:41:02 -0700

    Fixed Dashboard path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Dashboard from './Dashboard'
+import Dashboard from "./Dashboard";
 
 function App() {
-  return <Dashboard />
+  return <Dashboard />;
 }
 
-export default App
+export default App;
